c10033b69b491c08ea1395248d416cfa29d1608e
Zohaib Moti <zohaib.moti@amd.com>
2024-12-16 13:30:34 -0500

    copy static binaries to xco as well

--- promote.sh
+++ promote.sh
@@ -10,6 +10,10 @@ ARTIFACTS=(
 )
 
 for f in ${ARTIFACTS[@]}; do
-  scp staticx/$f zohaibm@xsjzohaibm44x:/proj/xsjhdstaff6/zohaibm/local/ffmpeg/bin/.
+  scp staticx/$f zohaibm@xsjzohaibm44x:/group/ngcodec/ma35_firmware/ffmpeg/bin/.
+  scp staticx/$f zohaibm@xcozohaibm41x:/proj/video_qa/MA35_QA/tools/ma35_firmware_tools/ffmpeg/bin/.
 done
 ssh zohaibm@xsjzohaibm44x "chmod a+rx /proj/xsjhdstaff6/zohaibm/local/ffmpeg/bin/*"
+ssh zohaibm@xcozohaibm41x "chmod a+rx /proj/video_qa/MA35_QA/tools/ma35_firmware_tools/ffmpeg/bin/*"
+
+
